3c380f939286e6477218c888ce6ece60d1f27c56
emretasar <tasaremre7@gmail.com>
2022-09-29 13:36:21 +0300

    Reverting to 39c08415f

--- server.py
+++ server.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-from flask import Flask
-=======
 from flask import Flask, render_template
 from datetime import datetime
->>>>>>> 39c08415f98f6334a3c68416f24303eb871c1984
 import views
 
 
@@ -12,14 +8,8 @@ def create_app():
 
     app.add_url_rule("/", view_func=views.home_page)
     app.add_url_rule("/movies", view_func=views.movies_page)
-<<<<<<< HEAD
 
     return app
-
-=======
-
-    return app
->>>>>>> 39c08415f98f6334a3c68416f24303eb871c1984
 
 if __name__ == "__main__":
     app = create_app()
