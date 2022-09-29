c860de51b33d2442a526ebd2ffc1796a85e4d397
emretasar <tasaremre7@gmail.com>
2022-09-29 13:05:36 +0300

    views.py added

--- server.py
+++ server.py
@@ -1,19 +1,17 @@
-from flask import Flask, render_template
-from datetime import datetime
+from flask import Flask
+import views
 
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route("/")
-def home_page():
-    today = datetime.today()
-    day_name = today.strftime("%A")
-    return render_template("home.html", day=day_name)
+    app.add_url_rule("/", view_func=views.home_page)
+    app.add_url_rule("/movies", view_func=views.movies_page)
 
-@app.route("/movies")
-def movies_page():
-    return render_template("movies.html")
+    return app
+
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(host="0.0.0.0", port=8080, debug=True)
 
