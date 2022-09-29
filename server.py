a8cdcce8d6ff0ea3837c065ff558accfe12f9ed6
emretasar <tasaremre7@gmail.com>
2022-09-29 10:36:06 +0300

    dynamic contents added

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return render_template("home.html")
+    today = datetime.today()
+    day_name = today.strftime("%A")
+    return render_template("home.html", day=day_name)
 
 
 if __name__ == "__main__":
